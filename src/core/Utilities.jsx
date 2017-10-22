cd6619d84f00f2aae369ab141d181c88e055253c
Nefaur Khandker <nefaurk@gmail.com>
2017-10-21 20:18:05 -0400

    Adding a Utilities class

---
+++ src/core/Utilities.jsx
@@ -0,0 +1,78 @@
+// Libraries
+import {StyleSheet, css} from 'aphrodite/no-important';
+
+export default class Utilities {
+	static getEmbeddedClassNames(className) {
+		let classNames = [];
+		if (className != null) {
+			if (className.constructor === String) {
+				classNames.push(className);
+			} else if (className instanceof Array) {
+				for (let element of className) {
+					classNames = classNames.concat(this.getEmbeddedClassNames(element));
+				}
+			}
+		}
+		return classNames;
+	}
+	static getEmbeddedStyles(style) {
+		let styles = [];
+		if (style != null) {
+			if (style instanceof Array) {
+				for (let element of style) {
+					const result = this.getEmbeddedStyles(element);
+					styles = styles.concat(result);
+				}
+			} else if (style instanceof Object) {
+				styles.push(style);
+			}
+		}
+		return styles;
+	}
+	static combinedClassNamesForComponent(component) {
+		let argumentsToPass = Array.prototype.splice.call(arguments, 1);
+		argumentsToPass.push(component.props.className);
+		return this.combinedClassNames.apply(this, argumentsToPass);
+	}
+	static combinedClassNames() {
+		const args = Array.from(arguments);
+		const classNames = this.getEmbeddedClassNames(args);
+		return classNames;
+	}
+	static combinedStylesForComponent(component) {
+		let argumentsToPass = Array.prototype.splice.call(arguments, 1);
+		argumentsToPass.push(component.props.style);
+		return this.combinedStyles.apply(this, argumentsToPass);
+	}
+	static combinedStyles() {
+		const args = Array.from(arguments);
+		const styles = this.getEmbeddedStyles(args);
+		return styles;
+	}
+	static combineStylingForComponent(component) {
+		let argumentsToPass = Array.prototype.splice.call(arguments, 1);
+		argumentsToPass = argumentsToPass.concat(this.getEmbeddedClassNames(component.props.className));
+		argumentsToPass = argumentsToPass.concat(this.getEmbeddedStyles(component.props.style));
+		// console.log("Styling:", argumentsToPass);
+		return this.combineStyling.apply(this, argumentsToPass);
+	}
+	static combineStyling() {
+		const args = Array.from(arguments);
+		let classNames = this.getEmbeddedClassNames(args);
+		const styles = this.getEmbeddedStyles(args);
+
+		const numStyles = styles.length;
+		let styleSheetDef = {};
+		for (let i = 0; i < numStyles; i++) {
+			styleSheetDef["style" + i] = styles[i];
+		}
+		const styleSheet = StyleSheet.create(styleSheetDef);
+		const styleSheetStyles = [];
+		for (let i = 0; i < numStyles; i++) {
+			styleSheetStyles.push(styleSheet["style" + i]);
+		}
+
+		classNames.push(css(styleSheetStyles));
+		return classNames.join(" ");
+	}
+}
